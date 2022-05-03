1186a937c09ff29ac80826335a13dac7746e581f
JKLLR <jeffhuria@gmail.com>
2022-05-03 17:50:05 +0300

    Created articles view function

--- app/views.py
+++ app/views.py
@@ -14,3 +14,14 @@ def index():
     tech_sources = get_sources('technology')
     print(tech_sources)
     return render_template('index.html', technology=tech_sources)
+    title = 'News Flow'
+    return render_template('index.html', title = title, technology=tech_sources)
+    
+@main.route('/source/<id>', ['GET', 'POST'])
+def articles(id):
+    """
+    function to return articles
+    """
+    articles = get_article(id)
+    title = f'Headline {id}'
+    return render_template('article.html', title = title, articles = articles) 
