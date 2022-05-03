43b188400dfd5d4cc9e6bee5464b163d2b218846
JKLLR <jeffhuria@gmail.com>
2022-05-03 12:46:53 +0300

    Passed in instance_relative_config to connect to instance folder

--- app/__init__.py
+++ app/__init__.py
@@ -6,9 +6,11 @@ from config import DevConfig
 #initialization of application
 
 app= Flask(__name__)
+app= Flask(__name__, instance_relative_config = True)
 
 
 #configuration set up
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 from app import views 
